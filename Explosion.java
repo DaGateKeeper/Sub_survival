d7fd6612cbc36af073b8b72e122ccfa07b1d5a6f
Emily Gallagher <emily.zoey.gallagher@gmail.com>
2019-04-06 15:54:43 -0400

    Explosion SFX.

--- Explosion.java
+++ Explosion.java
@@ -1,13 +1,17 @@
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
-// TAKEN FROM SPACE ROCKS animation might not work.
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.Gdx;
+
 public class Explosion extends BaseActor
 {
+    public static Sound eSFX = Gdx.audio.newSound(Gdx.files.internal("assets/audio/sfx/Explode.wav"));
 
     public Explosion(float x, float y, Stage stage)
     {
         super(x,y,stage);
         setAnimator( new Animator("assets/images/explosion.png", 5, 8, 0.02f, false) );
+        eSFX.play();
     }
     
     public void act(float deltaTime)
