e91927665c84c5a3548698c525e9f8f4d5457eb5
AMorales24 <42760280+AMorales24@users.noreply.github.com>
2019-04-06 14:03:29 -0400

    Add files via upload

--- InstructionScreen.java
+++ InstructionScreen.java
@@ -23,6 +23,20 @@ public class InstructionScreen extends BaseScreen
        BaseActor core = new BaseActor(0,0, mainStage);
        core.setAnimator( new Animator ("assets/images/the-core.png"));
        core.setSize(64,64);
+       
+       BaseActor bomb = new BaseActor(0,0, mainStage);
+       bomb.setAnimator( new Animator ("assets/images/bombOption3.jpg"));
+       bomb.setSize(64,64);
+       
+       BaseActor piercing = new BaseActor(0,0, mainStage);
+       piercing.setAnimator( new Animator ("assets/images/piercing.jpg"));
+       piercing.setSize(64,64);
+       
+       BaseActor rapidFire = new BaseActor(0,0, mainStage);
+       rapidFire.setAnimator( new Animator ("assets/images/rapidFire.jpg"));
+       rapidFire.setSize(64,64);
+
+
 
         Label title = new Label("Instructions", BaseGame.labelStyle);
         title.setFontScale(1.5f);
@@ -36,6 +50,16 @@ public class InstructionScreen extends BaseScreen
         
          Label powerup1 = new Label("Powerup gives +10 Ammo", BaseGame.labelStyle);
         powerup1.setColor( Color.CYAN );
+        
+         Label powerup2 = new Label("Powerup destroys all Subs", BaseGame.labelStyle);
+         powerup2.setColor( Color.CYAN );
+        
+        Label powerup3 = new Label("Powerup pierces Subs", BaseGame.labelStyle);
+        powerup3.setColor( Color.CYAN );
+        
+        Label powerup4 = new Label("Powerup allows Rapid-shots", BaseGame.labelStyle);
+        powerup4.setColor( Color.CYAN );
+        
         
         //Label controls = new Label("Arrow Keys Move Turtle", BaseGame.labelStyle2);
        // instructions.setColor( Color.CYAN);
@@ -52,15 +76,15 @@ public class InstructionScreen extends BaseScreen
         uiTable.add(tenMore);
         uiTable.add(powerup1);
         uiTable.row();
-       // uiTable.add(ballSmall);
-       // uiTable.add(smallballLabel).pad(10);
-       // uiTable.row();
-       // uiTable.add(ballLarge);
-       // uiTable.add(largeballLabel).pad(10);
-       // uiTable.row();
-       // uiTable.add(ballSpawn);
-       // uiTable.add(spawnballLabel).pad(10);
-        //uiTable.row();
+        uiTable.add(bomb);
+        uiTable.add(powerup2).pad(10);
+        uiTable.row();
+        uiTable.add(piercing);
+        uiTable.add(powerup3).pad(10);
+        uiTable.row();
+        uiTable.add(rapidFire);
+        uiTable.add(powerup4).pad(10);
+        uiTable.row();
         //uiTable.add(destroyPaddle);
        // uiTable.add(destroypaddleLabel).pad(10);
        // uiTable.row();
